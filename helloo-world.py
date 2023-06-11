ac5ec7c633ddd72663fec137f38bafb35e08c3c5
itsTofan <106501521+itsTofan@users.noreply.github.com>
2023-06-11 17:11:13 +0930

    for loop and variable

--- helloo-world.py
+++ helloo-world.py
@@ -1,1 +1,15 @@
-print("hello world!")
+
+friends = ['Taylor', 'Bamboo', 'Tofan', 'Alex']
+for friend in friends:
+    print ("Hi " + friend)
+
+for i in range (10):
+    print ("hello world")
+
+name = "Brook"
+print("Hello " + name)
+
+print(4+5)
+print(9*7)
+print(1/3)
+print(((1+2)*3)/4)
